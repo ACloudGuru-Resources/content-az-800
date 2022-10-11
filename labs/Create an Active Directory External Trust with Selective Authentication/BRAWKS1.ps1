920b06207ba67396f1680bc4b1d02843e45b4a0d
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-10-11 10:52:25 +1000

    updated scripts

--- labs/Create an Active Directory External Trust with Selective Authentication/BRAWKS1.ps1
+++ labs/Create an Active Directory External Trust with Selective Authentication/BRAWKS1.ps1
@@ -5,3 +5,6 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
 Install-WindowsFeature RSAT-AD-Tools -IncludeAllSubFeature
+$userName = 'cloud_user@corp.barrierreefaudio.com'
+[pscredential]$creds = New-Object System.Management.Automation.PSCredential ($userName, $pw)
+Add-Computer -Credential $creds -DomainName "corp.barrierreefaudio.com" -Restart -Force
