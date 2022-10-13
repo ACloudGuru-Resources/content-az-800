d381ce531e7e0160ea884481a797a307124f7180
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-10-13 12:30:00 +1000

    periodic commit

---
+++ labs/Configure Just Enough Administration (JEA)/BRAWKS1.ps1
@@ -0,0 +1,20 @@
+param(
+    $DomainName = 'corp.barrierreefaudio.com',
+    $UserName = 'admin_user',
+    $Password
+)
+# Speed Up Deployment
+$ProgressPreference = "SilentlyContinue"
+$WarningPreference = "SilentlyContinue"
+# Fix Server UI
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
+# Add Authenticated Users to the Remote Desktop Users Group
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member 'S-1-5-11'
+# Domain Join
+$pw = ConvertTo-SecureString "$($Password)" -AsPlainText -Force
+$userName = "$($UserName)@$($DomainName)"
+[pscredential]$creds = New-Object System.Management.Automation.PSCredential ($userName, $pw)
+Add-Computer -Credential $creds -DomainName "$($DomainName)" -Restart -Force
