f1333ba862816b3b155b3c586b37f36bfd5a8d01
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-01 12:30:56 +1000

    Periodic Commit

--- labs/Configure Hyper-V Switch-Embedded Teaming/BRAHV1.ps1
+++ labs/Configure Hyper-V Switch-Embedded Teaming/BRAHV1.ps1
@@ -16,32 +16,15 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 # Install Hyper-V
 Add-WindowsFeature Hyper-V -IncludeManagementTools -Restart:$false
 
-# Create NAT Virtual Switch
-Import-Module Hyper-V
-New-VMSwitch -Name 'InternalvSwitch' -SwitchType 'Internal'
-New-NetNat -Name LocalNAT -InternalIPInterfaceAddressPrefix “10.2.1.0/24”
-Get-NetAdapter "vEthernet (InternalvSwitch)" | New-NetIPAddress -IPAddress 10.2.1.1 -AddressFamily IPv4 -PrefixLength 24
+#Download Scripts
+New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri '' -OutFile 'C:\temp\Create-VM.ps1'
 
-# Create VHD
-$VM = "BRAVM1"
-New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "D:\Temp\$($VM).vhd" -Differencing
+#Set Scheduled Tasks
+# Remove Deployments
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VM.ps1 -Password $($Password)"
+$Trigger = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -TaskName "Create-VM" -Action $Action -Trigger $Trigger -Description "Clean-up Azure Deployments" -RunLevel Highest -User "System"
 
-# Download Answer File 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Configure%20Hyper-V%20Switch-Embedded%20Teaming/unattend.xml" -OutFile "C:\Temp\unattend.xml"
-
-# Inject Password into Answer File
-(Get-Content "C:\Temp\unattend.xml") -Replace '%LABPASSWORD%', "$($Password)" | Set-Content "C:\Temp\unattend.xml"
-
-#Download and Inject Answer File
-$Volume = Mount-VHD -Path "D:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
-New-Item "$($Volume.DriveLetter):\Windows" -Name "Panther" -ItemType Directory -ErrorAction "SilentlyContinue"
-Copy-Item "C:\Temp\unattend.xml" "$($Volume.DriveLetter):\Windows\Panther\unattend.xml"
-
-#Dismount the VHD
-Dismount-VHD -Path "D:\Temp\$($VM).vhd"
-
-# Create Virtual Machine
-New-VM -Name "$($VM)" -Generation 1 -MemoryStartupBytes 2GB -VHDPath "D:\Temp\$($VM).vhd" -SwitchName 'InternalvSwitch'
-Set-VMProcessor "$($VM)" -Count 2
-Set-VMProcessor "$($VM)" -ExposeVirtualizationExtensions $true
-Start-VM -VMName "$($VM)"
+#Restart the Server
+Restart-Computer -Force
