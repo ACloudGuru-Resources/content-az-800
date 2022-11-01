913790cf24d4ecbb8abfbdfa79a61b987bc25ed7
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-01 12:33:03 +1000

    Clean-up

--- labs/Configure Hyper-V Switch-Embedded Teaming/BRAHV1.ps1
+++ labs/Configure Hyper-V Switch-Embedded Teaming/BRAHV1.ps1
@@ -21,7 +21,6 @@ New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Configure%20Hyper-V%20Switch-Embedded%20Teaming/Create-VM.ps1' -OutFile 'C:\temp\Create-VM.ps1'
 
 #Set Scheduled Tasks
-# Remove Deployments
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VM.ps1 -Password $($Password)"
 $Trigger = New-ScheduledTaskTrigger -AtStartup
 Register-ScheduledTask -TaskName "Create-VM" -Action $Action -Trigger $Trigger -Description "Clean-up Azure Deployments" -RunLevel Highest -User "System"
