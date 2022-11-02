fda7b3a999763962d945a243623d71d9167c0acf
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-03 08:57:21 +1000

    Updates

--- labs/Deploy and Configure Windows Dynamic Host Configuration Protocol (DHCP) Servers/BRAHV1.ps1
+++ labs/Deploy and Configure Windows Dynamic Host Configuration Protocol (DHCP) Servers/BRAHV1.ps1
@@ -24,7 +24,8 @@ $VMs = @('BRADC1','BRAWKS1')
 foreach ($VM in $VMs) {
     #Set Scheduled Tasks to create the VM after restart
     $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VM.ps1 -Password $($Password) -VM $($VM)"
-    $Trigger = New-ScheduledTaskTrigger -AtStartup
+    # Random dleay so both don't run at exactly the same time
+    $Trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -Seconds 5)
     Register-ScheduledTask -TaskName "Create-VM $($VM)" -Action $Action -Trigger $Trigger -Description "Create VM" -RunLevel Highest -User "System"
 
     # Create VHD
--- labs/Deploy and Configure Windows Dynamic Host Configuration Protocol (DHCP) Servers/Create-VM.ps1
+++ labs/Deploy and Configure Windows Dynamic Host Configuration Protocol (DHCP) Servers/Create-VM.ps1
@@ -3,15 +3,18 @@ param(
     $Password,
     $VM
 )
-# Create NAT Virtual Switch
+# Import Hyper-V Module
 Import-Module Hyper-V
-New-VMSwitch -Name 'InternalvSwitch' -SwitchType 'Internal'
-New-NetNat -Name LocalNAT -InternalIPInterfaceAddressPrefix “10.2.1.0/24”
-Get-NetAdapter "vEthernet (InternalvSwitch)" | New-NetIPAddress -IPAddress 10.2.1.1 -AddressFamily IPv4 -PrefixLength 24
+
+# Create NAT Virtual Switch
+if (-not(Get-VMSwitch -Name "InternalvSwitch")) {
+    New-VMSwitch -Name 'InternalvSwitch' -SwitchType 'Internal'
+    New-NetNat -Name LocalNAT -InternalIPInterfaceAddressPrefix “10.2.1.0/24”
+    Get-NetAdapter "vEthernet (InternalvSwitch)" | New-NetIPAddress -IPAddress 10.2.1.1 -AddressFamily IPv4 -PrefixLength 24
+} 
 
 # Create VHD
-$VM = "BRAVM1"
-New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "D:\Temp\$($VM).vhd" -Differencing
+New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "C:\Temp\$($VM).vhd" -Differencing
 
 # Download Answer File 
 New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
@@ -20,7 +23,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/c
 # Inject Password into Answer File
 (Get-Content "C:\Temp\unattend.xml") -Replace '%LABPASSWORD%', "$($Password)" | Set-Content "C:\Temp\unattend.xml"
 
-#Download and Inject Answer File
+# Download and Inject Answer File
 $Volume = Mount-VHD -Path "C:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
 New-Item "$($Volume.DriveLetter):\Windows" -Name "Panther" -ItemType Directory -ErrorAction "SilentlyContinue"
 Copy-Item "C:\Temp\unattend.xml" "$($Volume.DriveLetter):\Windows\Panther\unattend.xml"
@@ -30,7 +33,6 @@ Dismount-VHD -Path "C:\Temp\$($VM).vhd"
 
 # Create Virtual Machine
 New-VM -Name "$($VM)" -Generation 1 -MemoryStartupBytes 2GB -VHDPath "C:\Temp\$($VM).vhd" -SwitchName 'InternalvSwitch'
-Add-VMNetworkAdapter -VMName BRAVM1 -SwitchName 'InternalvSwitch'
 Set-VMProcessor "$($VM)" -Count 2
 Set-VMProcessor "$($VM)" -ExposeVirtualizationExtensions $true
 
