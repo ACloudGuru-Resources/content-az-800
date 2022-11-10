6e26b2b76e43ade08a14f81097616732dda3e74c
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-10 13:48:59 +1000

    Updates

--- labs/Configure Nested Virtualization on an Azure Virtual Machine/BRAHV1.ps1
+++ labs/Configure Nested Virtualization on an Azure Virtual Machine/BRAHV1.ps1
@@ -15,6 +15,7 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 
 # Create VHD
 $VM = "BRAVM1"
+New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
 New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "D:\Temp\$($VM).vhd" -Differencing
 
 # Download Answer File 
@@ -25,12 +26,12 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/c
 (Get-Content "C:\Temp\unattend.xml") -Replace '%LABPASSWORD%', "$($Password)" | Set-Content "C:\Temp\unattend.xml"
 
 #Download and Inject Answer File
-$Volume = Mount-VHD -Path "D:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
+$Volume = Mount-VHD -Path "C:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
 New-Item "$($Volume.DriveLetter):\Windows" -Name "Panther" -ItemType Directory -ErrorAction "SilentlyContinue"
 Copy-Item "C:\Temp\unattend.xml" "$($Volume.DriveLetter):\Windows\Panther\unattend.xml"
 
 #Dismount the VHD
-Dismount-VHD -Path "D:\Temp\$($VM).vhd"
+Dismount-VHD -Path "C:\Temp\$($VM).vhd"
 
 # Install Hyper-V
 Add-WindowsFeature Hyper-V -IncludeManagementTools -Restart:$false
