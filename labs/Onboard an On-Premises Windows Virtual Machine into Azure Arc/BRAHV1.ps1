d4dd5764d62b5ba33aec55ca4f11a36b6b80b249
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-14 07:37:36 +1000

    Updates

--- labs/Onboard an On-Premises Windows Virtual Machine into Azure Arc/BRAHV1.ps1
+++ labs/Onboard an On-Premises Windows Virtual Machine into Azure Arc/BRAHV1.ps1
@@ -14,7 +14,7 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
 
 #Download Scripts
-Invoke-WebRequest -Uri '' -OutFile 'C:\temp\Create-VM.ps1'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Onboard%20an%20On-Premises%20Windows%20Virtual%20Machine%20into%20Azure%20Arc/Create-VM.ps1' -OutFile 'C:\temp\Create-VM.ps1'
 
 # Create VMs
 $VMs = @('BRAVM1')
