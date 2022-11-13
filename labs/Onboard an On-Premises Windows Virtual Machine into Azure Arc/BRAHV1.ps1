e653f0348e597a265a10d7d07a133b455e0303f7
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-14 07:36:18 +1000

    Updates

--- labs/Onboard an On-Premises Windows Virtual Machine into Azure Arc/BRAHV1.ps1
+++ labs/Onboard an On-Premises Windows Virtual Machine into Azure Arc/BRAHV1.ps1
@@ -13,34 +13,18 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
 
+#Download Scripts
+Invoke-WebRequest -Uri '' -OutFile 'C:\temp\Create-VM.ps1'
+
+# Create VMs
+$VMs = @('BRAVM1')
+foreach ($VM in $VMs) {
+    #Set Scheduled Tasks to create the VM after restart
+    $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VM.ps1 -Password $($Password) -VM $($VM)"
+    # Random dleay so both don't run at exactly the same time
+    $Trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -Seconds 5)
+    Register-ScheduledTask -TaskName "Create-VM $($VM)" -Action $Action -Trigger $Trigger -Description "Create VM" -RunLevel Highest -User "System"
+}
+
 # Install Hyper-V
-Add-WindowsFeature Hyper-V -IncludeManagementTools -Restart:$false
-
-# Create NAT Virtual Switch
-Import-Module Hyper-V
-New-VMSwitch -Name 'InternalvSwitch' -SwitchType 'Internal'
-New-NetNat -Name LocalNAT -InternalIPInterfaceAddressPrefix “10.2.1.0/24”
-Get-NetAdapter "vEthernet (InternalvSwitch)" | New-NetIPAddress -IPAddress 10.2.1.1 -AddressFamily IPv4 -PrefixLength 24
-
-# Create VHD
-$VM = "BRAVM1"
-New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "C:\Temp\$($VM).vhd" -Differencing
-
-# Download Answer File 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Onboard%20an%20On-Premises%20%20Windows%20Virtual%20Machine%20into%20Azure%20Arc/unattend.xml" -OutFile "C:\Temp\unattend.xml"
-
-# Inject Password into Answer File
-(Get-Content "C:\Temp\unattend.xml") -Replace '%LABPASSWORD%', "$($Password)" | Set-Content "C:\Temp\unattend.xml"
-
-#Download and Inject Answer File
-$Volume = Mount-VHD -Path "C:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
-New-Item "$($Volume.DriveLetter):\Windows" -Name "Panther" -ItemType Directory -ErrorAction "SilentlyContinue"
-Copy-Item "C:\Temp\unattend.xml" "$($Volume.DriveLetter):\Windows\Panther\unattend.xml"
-
-#Dismount the VHD
-Dismount-VHD -Path "C:\Temp\$($VM).vhd"
-
-# Create Virtual Machine
-New-VM -Name "$($VM)" -Generation 1 -MemoryStartupBytes 2GB -VHDPath "C:\Temp\$($VM).vhd" -SwitchName 'InternalvSwitch'
-Set-VMProcessor $($VM) -Count 2
-Start-VM -VMName "$($VM)"
+Add-WindowsFeature Hyper-V -IncludeManagementTools
