4f25ad6b8fb8a79b9f0909c09111dbebd7d0dd0d
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-14 12:13:52 +1000

    Updates

---
+++ labs/Implement IP Address Management (IPAM) on Windows Server/BRADC1.ps1
@@ -0,0 +1,25 @@
+param(
+    $Password
+)
+# Speed Up Deployment
+$ProgressPreference = "SilentlyContinue"
+$WarningPreference = "SilentlyContinue"
+
+# Fix Server UI
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
+
+# Network Location Awareness Fix for Single DC
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters" -Name "AlwaysExpectDomainController" -Value 1
+
+# Create Inactive Users
+$SecurePassword = ConvertTo-SecureString "$($Password)" -AsPlainText -Force
+New-LocalUser InactiveUser1 -Password $SecurePassword
+New-LocalUser InactiveUser2 -Password $SecurePassword 
+
+# Install ADDS and Create Forest
+Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
+$pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
+Install-ADDSForest -DomainName "corp.barrierreefaudio.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force
