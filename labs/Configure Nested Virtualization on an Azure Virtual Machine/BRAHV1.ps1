806188726c911f743ea736cdb33ac9e913209cb5
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-21 10:41:35 +1000

    Updates

--- labs/Configure Nested Virtualization on an Azure Virtual Machine/BRAHV1.ps1
+++ labs/Configure Nested Virtualization on an Azure Virtual Machine/BRAHV1.ps1
@@ -18,8 +18,7 @@ $VM = "BRAVM1"
 New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
 New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "C:\Temp\$($VM).vhd" -Differencing
 
-# Download Answer File 
-New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
+# Download Answer File
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Configure%20Nested%20Virtualization%20on%20an%20Azure%20Virtual%20Machine/unattend.xml" -OutFile "C:\Temp\unattend.xml"
 
 # Inject Password into Answer File
