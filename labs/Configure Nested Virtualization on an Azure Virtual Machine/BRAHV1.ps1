d43499297cc01691f9974bc5f37d4e5820e099ca
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-11-21 12:31:26 +1000

    Updates

--- labs/Configure Nested Virtualization on an Azure Virtual Machine/BRAHV1.ps1
+++ labs/Configure Nested Virtualization on an Azure Virtual Machine/BRAHV1.ps1
@@ -13,28 +13,17 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
 
+# Download Scripts
+New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Configure%20Nested%20Virtualization%20on%20an%20Azure%20Virtual%20Machine/Create-VHD.ps1' -OutFile 'C:\temp\Create-VHD.ps1'
+
+# Set Scheduled Tasks
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File C:\Temp\Create-VHD.ps1 -Password $($Password)"
+$Trigger = New-ScheduledTaskTrigger -AtStartup
+Register-ScheduledTask -TaskName "Create-VHD" -Action $Action -Trigger $Trigger -Description "Create VHD" -RunLevel Highest -User "System"
+
 # Install Hyper-V
 Add-WindowsFeature Hyper-V -IncludeManagementTools -Restart:$false
-
-# Create VHD
-$VM = "BRAVM1"
-New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
-New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "C:\Temp\$($VM).vhd" -Differencing
-
-# Download Answer File
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Configure%20Nested%20Virtualization%20on%20an%20Azure%20Virtual%20Machine/unattend.xml" -OutFile "C:\Temp\unattend.xml"
-
-# Inject Password into Answer File
-(Get-Content "C:\Temp\unattend.xml") -Replace '%LABPASSWORD%', "$($Password)" | Set-Content "C:\Temp\unattend.xml"
-
-#Download and Inject Answer File
-$Volume = Mount-VHD -Path "C:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
-New-Item "$($Volume.DriveLetter):\Windows" -Name "Panther" -ItemType Directory -ErrorAction "SilentlyContinue"
-Copy-Item "C:\Temp\unattend.xml" "$($Volume.DriveLetter):\Windows\Panther\unattend.xml"
-
-#Dismount the VHD
-Dismount-VHD -Path "C:\Temp\$($VM).vhd"
-
 
 #Restart the Server
 Restart-Computer -Force
---
+++ labs/Configure Nested Virtualization on an Azure Virtual Machine/Create-VHD.ps1
@@ -0,0 +1,22 @@
+param(
+    $Password
+)
+
+# Create VHD
+$VM = "BRAVM1"
+New-Item -Path C:\Temp -ItemType Directory -ErrorAction SilentlyContinue
+New-VHD -ParentPath "C:\Users\Public\Documents\20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd" -Path "C:\Temp\$($VM).vhd" -Differencing
+
+# Download Answer File
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ACloudGuru-Resources/content-az-800/master/labs/Configure%20Nested%20Virtualization%20on%20an%20Azure%20Virtual%20Machine/unattend.xml" -OutFile "C:\Temp\unattend.xml"
+
+# Inject Password into Answer File
+(Get-Content "C:\Temp\unattend.xml") -Replace '%LABPASSWORD%', "$($Password)" | Set-Content "C:\Temp\unattend.xml"
+
+#Download and Inject Answer File
+$Volume = Mount-VHD -Path "C:\Temp\$($VM).vhd" -PassThru | Get-Disk | Get-Partition | Get-Volume
+New-Item "$($Volume.DriveLetter):\Windows" -Name "Panther" -ItemType Directory -ErrorAction "SilentlyContinue"
+Copy-Item "C:\Temp\unattend.xml" "$($Volume.DriveLetter):\Windows\Panther\unattend.xml"
+
+#Dismount the VHD
+Dismount-VHD -Path "C:\Temp\$($VM).vhd"
