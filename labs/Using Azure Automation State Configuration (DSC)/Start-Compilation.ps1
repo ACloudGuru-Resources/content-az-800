8fda468f9a2ca83b17f9276a2c9a7d4b0942d9d6
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2024-10-28 07:26:24 +1000

    Updates

--- labs/Using Azure Automation State Configuration (DSC)/Start-Compilation.ps1
+++ labs/Using Azure Automation State Configuration (DSC)/Start-Compilation.ps1
@@ -8,4 +8,4 @@ $ConfigurationData = @{
 }
 $ResourceGroupName = Get-AzResourceGroup | Select-Object -ExpandProperty ResourceGroupName
 $AutomationAccountName = Get-AzAutomationAccount -ResourceGroupName $ResourceGroupName | Select-Object -ExpandProperty AutomationAccountName
-Start-AzAutomationDscCompilatiåonJob -ConfigurationName 'ServerConfiguration' -ConfigurationData $ConfigurationData -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName
+Start-AzAutomationDscCompilationJob -ConfigurationName 'ServerConfiguration' -ConfigurationData $ConfigurationData -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName 
