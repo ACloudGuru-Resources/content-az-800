c762082fb14f816c93f6b836f715b4751db51a38
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2024-10-28 07:30:05 +1000

    Updates

--- labs/Using Azure Automation State Configuration (DSC)/Start-Compilation.ps1
+++ labs/Using Azure Automation State Configuration (DSC)/Start-Compilation.ps1
@@ -1,11 +1,12 @@
 $ConfigurationData = @{
     AllNodes = @(
         @{
-            NodeName = "*"
-            PsDscAllowPlainTextPassword = $true
+            NodeName = '*'
+            PSDscAllowPlainTextPassword = $True
         }
     )
 }
+
 $ResourceGroupName = Get-AzResourceGroup | Select-Object -ExpandProperty ResourceGroupName
 $AutomationAccountName = Get-AzAutomationAccount -ResourceGroupName $ResourceGroupName | Select-Object -ExpandProperty AutomationAccountName
 Start-AzAutomationDscCompilationJob -ConfigurationName 'ServerConfiguration' -ConfigurationData $ConfigurationData -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName 
